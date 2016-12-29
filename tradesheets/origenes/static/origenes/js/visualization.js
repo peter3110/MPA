9fe859554c8fd67f8bbcb46d3a1767ecdfa4b5f2
peter3110 <peter3110>
2016-12-29 00:03:53 -0300

    mejoras al mapa

--- tradesheets/origenes/static/origenes/js/visualization.js
+++ tradesheets/origenes/static/origenes/js/visualization.js
@@ -14,30 +14,39 @@ function ready(error, land, cities) {
   map = d3.carto.map();
   d3.select("#viz").call(map);
 
+  // Limites entre los paises a considerar y datos sobre estos
   landLayer = d3.carto.layer.featureArray();
 
   landLayer
-  .features(topojson.feature(data.land, data.land.objects.world).features)
-  .label("Land")
-  .renderMode("svg")
-  .clickableFeatures(false)
-  .cssClass("land");
+    .features(topojson.feature(data.land, data.land.objects.world).features) // limites 
+    .label("Land")
+    .renderMode("svg")
+    .path("../static/origenes/data/world.geojson")  // != paises 
+    .clickableFeatures(true)
+    .cssClass("land");
 
+  // Ciudades marcadas en el mapa
   cityLayer = d3.carto.layer.xyArray();
 
   cityLayer
-  .features(data.cities)
-  .label("Cities")
-  .renderMode("svg")
-  .clickableFeatures(true)
-  .cssClass("city")
-  .markerSize(1)
-  .x("xcoord")
-  .y("ycoord")
+    .features(data.cities)
+    .label("Cities")
+    .renderMode("svg")
+    .clickableFeatures(true)
+    .cssClass("city")
+    .markerSize(1)
+    .x("xcoord")
+    .y("ycoord")
 
+  // Genero el mapa
   map
-  .addCartoLayer(landLayer)
-  .addCartoLayer(cityLayer)
-  .setScale(1);
+    .addCartoLayer(landLayer)
+    .addCartoLayer(cityLayer)
+    .setScale(1);
+
+  map.updateChoropleth({
+    USA: '#0fa0fa',
+    CAN: '#0fa0fa'
+  });
 
 }
