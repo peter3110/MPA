5b9ef99921123c3b3ccebbe498d72fbc2c66c601
peter3110 <peter3110>
2016-12-30 01:45:19 -0300

    agrego color de los oceanos

--- tradesheets/origenes/static/origenes/js/visualization.js
+++ tradesheets/origenes/static/origenes/js/visualization.js
@@ -2,17 +2,20 @@
 var data = {}, map;
 
 queue()
-  .defer(d3.json, "../static/origenes/data/land.topojson") // land
+  .defer(d3.json, "../static/origenes/data/countries.topojson") // land
   .defer(d3.csv, "../static/origenes/data/small_cities.csv") // cities
+  .defer(d3.json, "../static/origenes/data/oceans.json")
   .await(ready);
 
-function ready(error, land, cities) {
+function ready(error, countries, cities, oceans) {
 
-  data.land = land;
+  data.countries = countries;
   data.cities = cities;
+  data.oceans = oceans;
 
   map = d3.carto.map();
-  d3.select("#viz").call(map);
+  d3.select("#viz")
+    .call(map);
 
   // Ciudades marcadas en el mapa
   cityLayer = d3.carto.layer.xyArray();
@@ -27,24 +30,30 @@ function ready(error, land, cities) {
     .x("xcoord")
     .y("ycoord")
 
-  // Limites entre los paises a considerar y datos sobre estos
-  landLayer = d3.carto.layer.featureArray();
+  // Paises
+  countryLayer = d3.carto.layer.featureArray();
 
-  landLayer
-    .features(topojson.feature(data.land, data.land.objects.world).features) // limites 
-    .label("Land")
+  countryLayer
+    .features(topojson.feature(data.countries, data.countries.objects.world).features) // limites 
+    .label("Country")
     .renderMode("svg")
-    .cssClass("land");
-
-  landLayer
-    .path("../static/origenes/data/land.topojson")  // != paises clickeables
+    .cssClass("countries")
     .clickableFeatures(true);
 
-  // Genero el mapa
+  // Oceanos
+  oceansLayer = d3.carto.layer.featureArray();
+  oceansLayer
+    .features(topojson.feature(data.oceans, data.oceans.objects.ocean).features)
+    .label("Ocean")
+    .renderMode("svg")
+    .cssClass("oceans")
+    .clickableFeatures(false);
+
+  // Mapa
   map
-    .addCartoLayer(landLayer)
+    .addCartoLayer(countryLayer)
     .addCartoLayer(cityLayer)
-    //.addCartoLayer(countryLayer)
+    .addCartoLayer(oceansLayer)
     .mode("globe")
     .setScale(.1);
 
@@ -55,7 +64,7 @@ function ready(error, land, cities) {
   //landLayer.g().selectAll("path.land").style("fill", "pink");
   //colorScale = d3.scale.linear().domain([0,10000000,100000000,1000000000]).range(["gray","green","yellow","red"])
   //d3.selectAll("path.land").style("fill", function(d) {return colorScale(d.properties.pop)})
-  d3.selectAll("path.land").style("fill", function(d) {
+  d3.selectAll("path.countries").style("fill", function(d) {
     if (d.properties.iso == 'USA') {return "red"; }
     else if (d.properties.iso == 'ARG') {return "blue"; }
 
